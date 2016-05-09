897ed2901622cf2f1ef7b902d1c0d5afb6b598ed
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-05-09 14:38:42 +0200

    erste Übung hinzugefügt

---
+++ code/workspace/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: __dirname,
+        filename: "public/dist/main.js"
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/,  loader: 'babel'}
+        ]
+    },
+    devtool: 'source-map'
+
+};
