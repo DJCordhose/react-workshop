85d62e70f9bad6aafd158fb8fd5bfcbbaf48df11
Nils Hartmann <nils@nilshartmann.net>
2016-06-02 11:25:35 +0200

    Refactoring Test

--- code/schritte/3-tests/test/GreetingDetail.shallow.spec.js
+++ code/schritte/3-tests/test/GreetingDetail.shallow.spec.js
@@ -14,6 +14,38 @@ const greeting = {
 // TOOD split into several test cases as in GreetingDetail.jsdom.spec.js
 describe('GreetingDetail with SHALLOW RENDERING', () => {
     it('properly renders shallow', () => {
+
+        const renderer = ReactTestUtils.createRenderer();
+        renderer.render(
+            <GreetingDetail greeting={greeting} onAdd={noop} />
+        );
+        const tree = renderer.getRenderOutput();
+        expect(tree.type).toBe('div');
+        expect(tree.props.children.length).toEqual(4); // 2xinput ~ 2xbutton
+
+        const [nameInput, greetingInput] = tree.props.children;
+        expect(nameInput.props.value).toEqual('Lemmy');
+    })
+
+    it('changes input correctly with SHALLOW RENDERING', () => {
+        const renderer = ReactTestUtils.createRenderer();
+        renderer.render(
+            <GreetingDetail greeting={greeting} onAdd={noop} />
+        );
+        const tree = renderer.getRenderOutput();
+        const [nameInput, greetingInput] = tree.props.children;
+
+        nameInput.props.onChange({ target: { value: 'Klaus-Dieter' } });
+        greetingInput.props.onChange({ target: { value: 'Good morning' } });
+
+        const updatedTree = renderer.getRenderOutput();
+        const [updatedNameInput, updatedGreetingInput, , saveButton] = updatedTree.props.children;
+
+        expect(updatedNameInput.props.value).toEqual('Klaus-Dieter');
+        expect(updatedGreetingInput.props.value).toEqual('Good morning');
+    })
+
+    it('invokes onAdd callback when save button is clicked', () => {
         let greetingAdded;
         function onAddHandler(newGreeting) {
             greetingAdded = newGreeting;
@@ -24,23 +56,10 @@ describe('GreetingDetail with SHALLOW RENDERING', () => {
             <GreetingDetail greeting={greeting} onAdd={onAddHandler} />
         );
         const tree = renderer.getRenderOutput();
-        expect(tree.type).toBe('div');
-        expect(tree.props.children.length).toEqual(4); // 2xinput ~ 2xbutton
-
-        const [nameInput, greetingInput] = tree.props.children;
-        expect(nameInput.props.value).toEqual('Lemmy');
-
-        nameInput.props.onChange({ target: { value: 'Klaus-Dieter' } });
-        greetingInput.props.onChange({ target: { value: 'Good morning' } });
-
-        const updatedTree = renderer.getRenderOutput();
-        const [updatedNameInput, updatedGreetingInput, , saveButton] = updatedTree.props.children;
-
-        expect(updatedNameInput.props.value).toEqual('Klaus-Dieter');
-        expect(updatedGreetingInput.props.value).toEqual('Good morning');
+        const saveButton = tree.props.children[3];
 
         saveButton.props.onClick();
-        expect(greetingAdded).toEqual({ name: 'Klaus-Dieter', greeting: 'Good morning' });
+        expect(greetingAdded).toEqual(greeting);
     });
 
     it('renders two inputs and two buttons', () => {
