b0123e15b7373ba76515af917afef2274cf7ec6c
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-02-10 16:54:09 +0100

    ix workshop, work in progress

--- code/schritte/4-router/src/GreetingDisplay.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function GreetingDisplay(props) {
-    const {name, greeting} = props && props.location && props.location.query || {};
-    return <h1>{greeting}, {name}</h1>;
-}
