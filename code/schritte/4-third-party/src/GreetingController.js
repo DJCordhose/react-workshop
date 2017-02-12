2ff28fd61584ae4d5685d539400ade68841f82c9
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-02-12 10:53:25 +0100

    3rd party integration, including callback from chart doing filtering

--- code/schritte/4-third-party/src/GreetingController.js
+++ code/schritte/4-third-party/src/GreetingController.js
@@ -24,21 +24,29 @@ const MODE_DETAIL = 'MODE_DETAIL';
 
 export default class GreetingController extends React.Component {
     render() {
-        const {mode, greetings} = this.state;
+        const {mode, greetings, filter} = this.state;
         const aggregatedGreetings = aggregateGreetings(greetings);
+        const filtered = filter ? greetings.filter(greeting => greeting.name === filter) : greetings;
 
         return (
             <div>
                 <div>
                     {mode === MODE_MASTER ?
-                        <GreetingMaster greetings={greetings}
+                        <GreetingMaster greetings={filtered}
                                         onAdd={() => this.setState({mode: MODE_DETAIL})}
                         /> :
                         <GreetingDetail onAdd={(greeting) => this.addGreeting(greeting)}/>
                     }
                 </div>
                 <div>
-                    <Chart data={aggregatedGreetings}/>
+                    <Chart data={aggregatedGreetings} onSegmentSelected={filter => {
+                        if (this.state.filter === filter) {
+                            // reset filter when clicking again
+                            this.setState({filter: null})
+                        } else {
+                            this.setState({filter})
+                        }
+                    }}/>
                 </div>
             </div>);
     }
@@ -47,7 +55,8 @@ export default class GreetingController extends React.Component {
         super(props);
         this.state = {
             greetings: sampleGreetings,
-            mode: MODE_MASTER
+            mode: MODE_MASTER,
+            filter: null
         };
     }
 
