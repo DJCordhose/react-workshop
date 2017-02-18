8248e0e41561089e90718321c3654ba52906f1f2
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-02-18 19:06:34 +0100

    added dev tools

--- code/experiment/mobx/src/main.js
+++ code/experiment/mobx/src/main.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DevTools from 'mobx-react-devtools'
 
 import GreetingController from './GreetingController';
 import store from './store';
-
 const mountNode = document.getElementById('mount');
-ReactDOM.render(<GreetingController store={store}/>, mountNode);
+ReactDOM.render(
+<div>
+    <GreetingController store={store}/>
+    <DevTools />
+</div>, mountNode);
