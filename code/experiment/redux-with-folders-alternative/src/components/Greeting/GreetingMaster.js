94aa093af522300b3156afddb12ae920b0c1e5e5
Nils Hartmann <nils@nilshartmann.net>
2017-02-19 13:35:27 +0100

    Add filter info and reset to GreetingMaster (backported from step 5)

--- code/experiment/redux-with-folders-alternative/src/components/Greeting/GreetingMaster.js
+++ code/experiment/redux-with-folders-alternative/src/components/Greeting/GreetingMaster.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 import * as actions from '../../actions';
 import {filterGreetings} from '../../selectors';
 import {MODE_DETAIL} from '../../actions';
 
+
+const GreetingRow = ({greeting, onRowClicked}) => (
+    <tr onClick={() => onRowClicked(greeting)}>
+        <td>{greeting.name}</td>
+        <td>{greeting.greeting}</td>
+    </tr>
+);
+
+const FilterPanel = ({filter, setFilter}) => {
+    if (!filter) {
+        return <div className="FilterPanel">(All greetings are shown. Click a row to filter)</div>;
+    }
+
+    return <div className="FilterPanel">(Shown are greetings for <b>{filter}</b>. <a
+        onClick={() => setFilter(null)}>Reset Filter</a>)</div>
+};
+
+
 function GreetingMaster(props) {
-    const {greetings, onAdd} = props;
-    const body = greetings.map(greeting => <tr key={greeting.id}><td>{greeting.name}</td><td>{greeting.greeting}</td></tr>);
+    const {greetings, setMode, filter, setFilter} = props;
+    const body = greetings.map(greeting => <GreetingRow key={greeting.id} greeting={greeting}
+                                                        onRowClicked={greeting => setFilter(greeting.name)}/>);
     return (
         <div>
             <table>
                 <thead>
-                    <tr><th>Name</th><th>Greeting</th></tr>
+                <tr>
+                    <th>Name</th>
+                    <th>Greeting</th>
+                </tr>
                 </thead>
                 <tbody>
                 {body}
                 </tbody>
             </table>
-            <button className="ActionButton"
-                onClick={onAdd}>
-                +
+            <FilterPanel filter={filter} setFilter={setFilter}/>
+            <button
+                onClick={()=>setMode(MODE_DETAIL)}>
+                Add
             </button>
         </div>
     );
@@ -29,9 +53,7 @@ function GreetingMaster(props) {
 export default connect(
     state => ({
         greetings: filterGreetings(state.greetings, state.filter),
-        mode: state.mode
+        filter: state.filter
     }),
-    dispatch => ({
-        onAdd: () => dispatch(actions.setMode(MODE_DETAIL))
-    })
+    dispatch => bindActionCreators(actions, dispatch)
 )(GreetingMaster);
