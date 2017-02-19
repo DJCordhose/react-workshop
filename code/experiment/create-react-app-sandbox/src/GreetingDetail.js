defbb9dcf43afe03230ae4f573565d55161d0f0d
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-02-19 10:07:21 +0100

    added create-react-app version

---
+++ code/experiment/create-react-app-sandbox/src/GreetingDetail.js
@@ -0,0 +1,60 @@
+import React, {PropTypes} from 'react';
+
+export default class GreetingDetail extends React.Component {
+    render() {
+        const {name, greeting} = this.state;
+
+        return (
+            <div>
+                <input ref={input => this.input = input}
+                       onChange={event => this.updateModel('name', event.target.value)}
+                       value={name}
+                       placeholder="Name"/>
+                <input onChange={event => this.updateModel('greeting', event.target.value)}
+                       value={greeting}
+                       placeholder="Greeting"/>
+                <button
+                    onClick={() => this.reset()}>
+                    Clear
+                </button>
+                <button
+                    onClick={() => this.save()}>
+                    Save
+                </button>
+            </div>);
+    }
+
+    constructor(props) {
+        super(props);
+        const {name, greeting} = this.props.greeting || {name: '', greeting: ''};
+        this.state = {
+            name,
+            greeting
+        };
+    }
+
+    reset() {
+        this.setState({name: '', greeting: ''});
+        this.input.focus();
+    }
+
+    save() {
+        const {onAdd} = this.props;
+        const {name, greeting} = this.state;
+        onAdd({
+            name,
+            greeting
+        });
+    }
+
+    updateModel(name, value) {
+        this.setState({[name]: value});
+    }
+}
+GreetingDetail.propTypes = {
+    greeting: PropTypes.shape({
+        name: React.PropTypes.string.isRequired,
+        greeting: React.PropTypes.string.isRequired
+    }),
+    onAdd: PropTypes.func.isRequired
+};
