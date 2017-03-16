75fa94e239a01b2f58a8321be4b7de1799b76142
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-03-16 12:59:13 +0100

    complete version of react router

--- code/sandbox/my-create-react-app/src/index.js
+++ code/sandbox/my-create-react-app/src/index.js
@@ -4,14 +4,24 @@ require('whatwg-fetch');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GreetingController from './GreetingController';
 
 import './normalize.css';
 import './styles.css';
 import './lib/nv.d3.css';
 import './google-fonts.css';
 
-ReactDOM.render(
-    <GreetingController />,
-  document.getElementById('root')
-);
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+import GreetingController from './GreetingController';
+import GreetingDisplay from './GreetingDisplay';
+import Layout from './Layout';
+
+const mountNode = document.getElementById('root');
+const router = <Router history={hashHistory }>
+    <Route path="/" component={Layout}>
+        <IndexRoute component={GreetingController} />
+        <Route path="greet" component={GreetingDisplay} />
+    </Route>
+</Router>;
+
+ReactDOM.render(router, mountNode);
