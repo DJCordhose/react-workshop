516fc2539f276ef2d9f6733c6e38b1f4d81b933d
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-03-16 10:15:23 +0100

    added callback examples

--- code/sandbox/callbacks.js
+++ code/sandbox/callbacks.js
@@ -1,11 +1,33 @@
 // Excursion: callbacks, this, and arrow functions
 
-function caller(callback) {
-    callback("Parameter from Caller");
-}
-
 function simple(param) {
     console.log(`Simple: ${param}`);
 }
 
-caller(simple);
+// caller(simple());
+// caller(simple);
+
+function caller(callback) {
+    // console.log(callback);
+    callback("Parameter from Caller");
+}
+
+class Researcher {
+
+    constructor(name) {
+        this.name = name;
+    }
+    research(param) {
+        console.log(`${this.name} is researching, param was ${param}`);
+    }
+    doCallback() {
+        // this.research('Param from doCallback');
+        let callback = (param) => this.research(param);
+        caller(callback);
+    }
+}
+
+const r = new Researcher('Albert');
+r.doCallback();
+// r.research();
+
