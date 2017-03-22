0fb8790ba7b41d63b3524bf35db2f9ac1ef9b706
Nils Hartmann <nils@nilshartmann.net>
2017-03-22 21:41:16 +0100

    Migrate to React Router v4, pt 2: remove MODE from GreetingController

--- code/schritte/router/src/Layout.js
+++ code/schritte/router/src/Layout.js
@@ -2,15 +2,16 @@ import React from "react";
 
 import GreetingController from "./GreetingController";
 import GreetingDisplayController from "./GreetingDisplayController";
-import {HashRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 
-const Layout = props => (
+const Layout = () => (
     <Router>
         <div>
             <h1>Greetings</h1>
-            <Route exact path="/" component={GreetingController}/>
-            <Route path="/greet/:greetingId" component={GreetingDisplayController}/>
-            {props.children}
+            <Switch>
+                <Route path="/greet/:greetingId" component={GreetingDisplayController}/>
+                <Route path="/" component={GreetingController}/>
+            </Switch>
         </div>
     </Router>
 );
--- code/schritte/router/src/main.js
+++ code/schritte/router/src/main.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 
 import Layout from "./Layout";
 
-
 const mountNode = document.getElementById('mount');
 ReactDOM.render(<Layout />, mountNode);
