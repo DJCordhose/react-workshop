5d54b577202f43e6d26fc5e7ee36162cb763b2fb
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-03-28 17:18:13 +0200

    tagged unions for actions

--- code/sandbox/typescript/src/actions.ts
+++ code/sandbox/typescript/src/actions.ts
@@ -1,11 +1,25 @@
-export function updateGreeting(greeting) {
+export const RESET_GREETING = 'RESET_GREETING';
+export const UPDATE_GREETING = 'UPDATE_GREETING';
+
+export type UpdateGreetingAction = {
+    type: typeof UPDATE_GREETING;
+    payload: string;
+};
+
+export type ResetGreetingAction = {
+    type: typeof RESET_GREETING;
+};
+
+export type Action = UpdateGreetingAction | ResetGreetingAction;
+
+export function updateGreeting(greeting: string): UpdateGreetingAction {
     return {
-        type: 'UPDATE_GREETING',
+        type: UPDATE_GREETING,
         payload: greeting
     };
 }
 
-export function resetGreeting() {
+export function resetGreeting(): ResetGreetingAction {
     return {
         type: 'RESET_GREETING'
     };
