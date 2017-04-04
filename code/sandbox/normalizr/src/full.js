1e36f70206c2b9739bf5c795f3221cd2bfea5d85
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-04-04 19:06:16 +0200

    complete normalizr example

--- code/sandbox/normalizr/src/full.js
+++ code/sandbox/normalizr/src/full.js
@@ -14,23 +14,28 @@ const comment = new schema.Entity('comments', {
 const article = new schema.Entity('articles', {
   author: user,
   comments: [ comment ]
+},{
+    idAttribute: 'name'
 });
 
-const blog = new schema.Entity('blog', {
-  articles: [ article ]
-}, {
-  idAttribute: 'blogname'
-});
+const mySchema = { articles: [ article ] };
 
-const normalizedData = normalize(originalData, blog);
+const normalizedData = normalize(originalData, mySchema);
 
 console.log(`Normalized: ${JSON.stringify(normalizedData, null, '  ')}`);
+console.log(`Entities: ${JSON.stringify(normalizedData.entities, null, '  ')}`);
 
 const paul = normalizedData.entities.user["1"];
 paul.name = 'Paul II';
 
 console.log(`Paul: ${JSON.stringify(paul, null, '  ')}`);
 
-const denormalizedData = denormalize({user: "1"},  user , normalizedData.entities);
+const input = {
+    "articles": [
+        "strange"
+    ]
+};
+
+const denormalizedData = denormalize(input,  mySchema, normalizedData.entities);
 
 console.log(`Denormalized: ${JSON.stringify(denormalizedData, null, '  ')}`);
