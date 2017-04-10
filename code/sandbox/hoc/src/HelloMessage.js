a42fe6ee4370e8318d68867874ebdbe874617e50
Oliver Zeigermann <oliver.zeigermann@xing.com>
2017-04-10 11:52:00 +0200

    created a simple HoC example

--- code/sandbox/hoc/src/HelloMessage.js
+++ code/sandbox/hoc/src/HelloMessage.js
@@ -1,28 +1,32 @@
 import React from 'react';
+import {withState} from './stateHoc';
 
-export default class HelloMessage extends React.Component {
+// https://facebook.github.io/react/docs/react-api.html#react.purecomponent
+// might cause performance boost
+// might be functional component instead
+class HelloMessage extends React.PureComponent {
     render() {
+        const {value, updateModel} = this.props;
         return (
             <div>
                 <input ref={input => this.input = input}
-                       onChange={event => this.updateModel(event)}
-                       value={this.state.greeting} />
-                <p>{this.state.greeting}, World</p>
+                       onChange={event => updateModel(event.currentTarget.value)}
+                       value={value} />
+                <p>{value}, World</p>
                 <button
                     onClick={() => this.reset()}>
                     Clear
                 </button>
             </div>);
     }
-    constructor(props) {
-        super(props);
-        this.state = {greeting: this.props.greeting};
-    }
     reset() {
-        this.setState({greeting: ""});
+        const {updateModel} = this.props;
+        updateModel('');
         this.input.focus();
     }
-    updateModel(event) {
-        this.setState({greeting: event.target.value});
-    }
 }
+
+// https://facebook.github.io/react/docs/higher-order-components.html
+export default withState(HelloMessage, {
+  initialDataProp: 'greeting'
+});
--- code/sandbox/hoc/src/index.js
+++ code/sandbox/hoc/src/index.js
@@ -5,7 +5,3 @@ import HelloMessage from './HelloMessage';
 
 const mountNode = document.getElementById('mount');
 ReactDOM.render(<HelloMessage greeting="Hello"/>, mountNode);
-
-// TODO
-// - simpleHOC: Extracting state to HOC
-// - withBreakpoint: for XING
