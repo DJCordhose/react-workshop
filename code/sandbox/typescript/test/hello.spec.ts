e3be5464e491c1c7305f59202dcfda6c4dcb7d48
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-04-12 16:55:53 +0200

    added mocha declarations

--- code/sandbox/typescript/test/hello.spec.ts
+++ code/sandbox/typescript/test/hello.spec.ts
@@ -1,10 +1,9 @@
-// import {describe, it} from 'mocha';
 import {assert} from 'chai';
 
 import {resetGreeting, RESET_GREETING} from '../src/actions';
 
 describe('Main', () => {
-  it('says hello properly 2', () => {
+  it('says hello properly', () => {
     const action = resetGreeting();
     assert(action.type === RESET_GREETING, 'aha2')
   });
