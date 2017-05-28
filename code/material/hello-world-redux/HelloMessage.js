4c8627c2edac6cd77dcc29e8cb272548ed7d5411
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-05-28 21:48:42 +0200

    more elaborated redux hello world

--- code/material/hello-world-redux/HelloMessage.js
+++ code/material/hello-world-redux/HelloMessage.js
@@ -1,49 +1,23 @@
 import React from 'react';
-import { connect } from 'react-redux';
-
-const clearGreeting = () => {
-  return {
-    type: 'CLEAR'
-  }
-};
-
-const updateGreeting = (greeting) => {
-  return {
-    type: 'UPDATE',
-    greeting
-  }
-};
+import {connect} from 'react-redux';
+import {resetGreeting, updateGreeting} from './actions';
 
 class HelloMessage extends React.Component {
     render() {
-      const {greeting, clearGreeting, updateGreeting} = this.props;
+        const {updateGreeting, resetGreeting, greeting} = this.props;
         return (
             <div>
-                <input ref={input => this.input = input}
-                       onChange={event => updateGreeting(event.target.value)}
+                <input onChange={event => updateGreeting(event.currentTarget.value)}
                        value={greeting} />
                 <p>{greeting}, World</p>
                 <button
-                    onClick={() => this.reset()}>
+                    onClick={resetGreeting}>
                     Clear
                 </button>
             </div>);
     }
-    reset() {
-      const {greeting, clearGreeting, updateGreeting} = this.props;
-        this.props.clearGreeting();
-        this.input.focus();
-    }
 }
 
-const selectGreetingFromState = (state) => state.greeting;
-
-export default connect(
-  state => ({
-    greeting: selectGreetingFromState(state)
-  }),
-  {
-    clearGreeting,
-    updateGreeting
-  }
-)(HelloMessage);
+export default connect(state => ({
+    greeting: state.greeting
+}), {updateGreeting, resetGreeting})(HelloMessage);
