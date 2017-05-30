23d09e3cdc0eccd22562cc75d2999139f7a51d8b
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-05-30 18:02:34 +0200

    async example: added error handling and async arrow functions

--- code/sandbox/async-await.js
+++ code/sandbox/async-await.js
@@ -1,9 +1,13 @@
-// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
 
+let count = 0;
 function resolveAfter2Seconds(x) {
     return new Promise(resolve => {
         setTimeout(() => {
-            resolve(x);
+            count++;
+            console.log(`Count: ${count}`)
+            resolve(x + count);
         }, 2000);
     });
 }
@@ -14,9 +18,9 @@ async function add1(x) {
     return x + await a + await b;
 }
 
-add1(10).then(v => {
-    console.log(v);  // prints 60 after 2 seconds.
-});
+// add1(10).then(v => {
+//     console.log(v);  // prints 60 after 2 seconds.
+// });
 
 async function add2(x) {
     const a = await resolveAfter2Seconds(20);
@@ -24,14 +28,36 @@ async function add2(x) {
     return x + a + b;
 }
 
-add2(10).then(v => {
-    console.log(v);  // prints 60 after 4 seconds.
-});
+// add2(10).then(v => {
+//     console.log(v);  // prints 60 after 4 seconds.
+// });
+
+const add3 = async x => x + await resolveAfter2Seconds(20) + await resolveAfter2Seconds(30);
+
+// add3(10).then(v => {
+//     console.log(v);  // prints 60 after 4 seconds.
+// });
+
+function rejectAfter2Seconds(x) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            count++;
+            console.log(`Count: ${count}`)
+            reject(x + count);
+        }, 2000);
+    });
+}
 
 
-async function main() {
-    const res = await add2(20);
-    console.log(res);
+async function main() {    
+    try {
+        const add4 = async x => await rejectAfter2Seconds(30);
+
+        const result = await add4(10);
+        console.log(result);
+    } catch (unhandled) {
+        console.warn(unhandled);
+    }
 }
 
 main();
