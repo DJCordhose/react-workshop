bda2fdabbd1f248dfa17952b3dbdcbf09ad0b3ad
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-05-31 12:01:16 +0200

    async/await: added remote step

---
+++ code/schritte/3-remote-async/src/main.js
@@ -0,0 +1,13 @@
+// polyfill for IE
+require('es6-promise').polyfill();
+require('whatwg-fetch');
+// needed for async / await
+require("babel-polyfill");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GreetingController from './GreetingController';
+
+const mountNode = document.getElementById('mount');
+ReactDOM.render(<GreetingController />, mountNode);
