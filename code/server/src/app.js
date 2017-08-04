6bb0c4665476a93e48634ef9481d2096abde3720
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-08-04 12:29:02 +0200

    cors for all hosts (just in case client does not start on 8080)

--- code/server/src/app.js
+++ code/server/src/app.js
@@ -7,7 +7,7 @@ const createApp = db => {
 	app.use(bodyParser.json());
 
 	app.use((req, res, next) => {
-		res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		res.header("Access-Control-Allow-Credentials", "true");
 		next();
